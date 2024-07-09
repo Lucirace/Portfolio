e77fe52ef38d76b9ae4f559a8228daa1541faa6f
Nipun Agarwal <agarwalnipun19802@gmail.com>
2024-07-09 18:07:38 +0530

    Update info.js

--- info.js
+++ info.js
@@ -9,16 +9,15 @@ let info = {
     },
   },
   description:
-    "A second year Computer Science graduate student at Indiana University Bloomington with an interest in Software Development, UX Design and Artificial Intelligence - and seeking exciting opportunities related to the same.<br><br>I am a detail oriented person and I keep my goals and tasks organized to maintain productivity. I would bring to table a wide range of software skills, ranging from design and development to writing scripts for intelligent agents. I believe that the skills that I have attained from the dynamic environment and competitive university life define me, and I am looking forward to implementing and polishing them in any opportunity that I receive. ",
+    "I'm a Backend-Full Stack Developer and an undergraduate student pursuing a Bachelor's in Electronics & Communication Engineering from UIET Panjab University Chandigarh.<br><br>I have a strong foundation in frameworks like Node.js, React, PostgreSQL, and TypeScript.I have also worked on several personal projects, exhibiting my knowledge in implementing authentication, database management, and cloud deployment.I also did graphic designing for my college organizations. Currently, I am learning  Prometheus, Grafana, Kubernetes, and more for a trading application as a side project.With a strong passion for technology and a commitment to continuous learning, I am excited to contribute to innovative projects and collaborate with like-minded individuals in software development. ",
   links: {
     linkedin:
       "https://www.linkedin.com/in/nipunagarwal1144?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bd9AM69hvRNCE4Q%2FkQRhlxg%3D%3D",
     github: "https://github.com/Lucirace",
     googledev: "https://g.dev/nipun_agarwal",
-    resume:
-      "https://github.com/hrishikeshpaul/portfolio-template/blob/master/src/assets/pdfs/Resume.pdf",
-    twitter: "https://g.dev/nipun_agarwal",
-    instagram: "https://g.dev/nipun_agarwal",
+    resume: "  ",
+    twitter: "https://twitter.com/nipunAgarwal404",
+    instagram: "https://www.instagram.com/nipunagarwal._/?next=%2F",
   },
   education: [
     {
@@ -109,205 +108,257 @@ let info = {
     {
       title: "Languages",
       info: [
-        "Python",
-        "Javascript",
-        "TypeScript",
-        "Java",
-        "C",
         "C++",
+        "TypeScript",
+        "JavaScript",
+        "C",
+        "SQL (Postgres)",
         "SQL",
-        "NoSQL",
-        "Ruby",
+        "HTML",
+        "CSS ",
       ],
       icon: "fa fa-code",
     },
     {
-      title: "Data frameworks",
+      title: "Frameworks",
       info: [
-        "Keras",
-        "Numpy",
-        "Pandas",
-        "OpenCV",
-        "SciPy",
-        "Sklearn",
-        "LibROSA",
-        "Pillow",
+        "React",
+        "Node.js",
+        "Express.js",
+        "Next.js",
+        "Hono",
+        "Cloudflare Workers",
       ],
       icon: "fa fa-cubes",
     },
     {
       title: "Web Technologies",
-      info: ["Vue", "Angular", "React", "Node", "Flask", "HTML", "CSS"],
+      info: [
+        "REST",
+        "Docker",
+        "Google Cloud Platform",
+        "Recoil",
+        "Redis",
+        "Web Sockets",
+        "Open API Spec",
+        "Turborepo",
+        "Tailwind CSS",
+        "shadcn/ui",
+        "Gemini API",
+        "Hive Blockchain",
+      ],
       icon: "fas fa-laptop-code",
     },
     {
       title: "Databases",
-      info: ["MongoDB", "MySQL", "OracleSQL", "Neo4j"],
+      info: ["MongoDB", "PostgreSQL", "Prisma ORM", "Drizzle ORM"],
       icon: "fa fa-database",
     },
     {
       title: "Operating systems & Tools",
       info: [
-        "MacOS",
-        "Ubuntu",
+        "Git",
+        "GitHub Action",
+        "CI/CD pipeline",
+        "Postman",
+        "Vim",
+        "Nginx",
+        "Wrangler CLI",
+        "WSL",
         "Windows",
-        "Android",
-        "Agile",
-        "Scrum",
-        "JIRA",
-        "Heroku",
-        "Firebase",
-        "AWS S3",
+        "AWS EC2",
       ],
       icon: "fas fa-tools",
     },
     {
       title: "Design",
-      info: ["Illustrator", "XD", "Photoshop", "Final Cut Pro"],
+      info: ["Illustrator", "Canva", "Photoshop", "Final Cut Pro"],
       icon: "fa fa-pencil-square-o",
     },
   ],
   portfolio: [
     {
-      name: "Sheet Recognizer",
+      name: "Bharat Wallet",
       pictures: [
         {
-          img: require("./src/assets/portfolio/msr/one.png"),
+          img: require("./src/assets/portfolio/msr/wallet1.png"),
         },
         {
-          img: require("./src/assets/portfolio/msr/two.png"),
+          img: require("./src/assets/portfolio/msr/wallet2.png"),
         },
         {
-          img: require("./src/assets/portfolio/msr/three.png"),
+          img: require("./src/assets/portfolio/msr/wallet3.png"),
         },
         {
-          img: require("./src/assets/portfolio/msr/four.png"),
-        },
-      ],
-      technologies: ["Computer Vision", "OpenCV", "AI", "Flask", "Sockets"],
-      category: "Algorithm",
-      date: "Jan, 2020 - Mar, 2020",
-      github:
-        "https://github.com/hrishikeshpaul/music-sheet-recognizer-backend",
-      visit: "https://github.com/hrishikeshpaul/music-sheet-recognizer-backend",
-      description:
-        "Music Sheet Recognizer (MSR) highlights basic Image Processing and Recognition. Like Optical Character Recognition, this tool is used to detect music notes and rests. It makes use of basic Computer Vision techniques such as Convolution, Edge Detections, Hough Transforms and Template Matching to detect notes and rests on a music sheet. ",
-    },
-    {
-      name: "NoQ Job Portal",
-      pictures: [
-        {
-          img: require("./src/assets/portfolio/noq/logo.png"),
+          img: require("./src/assets/portfolio/msr/wallet4.png"),
         },
         {
-          img: require("./src/assets/portfolio/noq/one.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/noq/two.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/noq/three.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/noq/four.png"),
-        },
-      ],
-      technologies: ["Node", "Vue", "Express", "MongoDB", "Heroku"],
-      category: "Website",
-      date: "Sep, 2019 - Dec  , 2019",
-      github: "https://github.com/hrishikeshpaul/noq",
-      visit: "https://noq-client.herokuapp.com/",
-      description:
-        "No Q, is a revolutionary tool which allows for both employers and students to skip the hassle seen in modern day career fairs. For the students, there is an intuitive profile builder where you can add things like education, skills, certifications, honors, and experiences. No Q is not only good for students, but employers as well! A common problem at modern career fairs is the number of unqualified candidates who waste their time when they could be chatting with someone else. With No Q, only the students who meet the positions’ criteria will be able to apply, and from there the recruiter can invite them to talk in more detail at the career fair through our intuitive messaging system.",
-    },
-    {
-      name: "POST",
-      pictures: [
-        {
-          img: require("./src/assets/portfolio/post/one.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/post/two.png"),
+          img: require("./src/assets/portfolio/msr/wallet5.png"),
         },
       ],
       technologies: [
-        "Markov Models",
-        "Python",
-        "Artificial Intelligence",
-        "Vue",
-        "Heroku",
+        "Docker",
+        "AWS",
+        "Turborepo",
+        "Node.js",
+        "Typescript",
+        "Next.js",
+        "PostgreSQL",
+        "Tailwind CSS",
       ],
-      category: "Algorithm",
-      github: "https://github.com/hrishikeshpaul/pos-tagger",
-      date: "Oct, 2019 - Nov, 2019",
-      visit: "https://post-client.herokuapp.com/",
-      description:
-        "Post is a simple algorithm that was developed to tagging a word corresponding to its part of speech. The algorithm makes of a probabilisitic approach along with some randomness, together which forms the basis of an algorithm called Gibbs Sampling.",
-    },
-    {
-      name: "Suicide Analyzer",
-      pictures: [
-        {
-          img: require("./src/assets/portfolio/suicide/suicide-analyzer.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/suicide/suicide2-original.png"),
-        },
-      ],
-      technologies: ["Node", "Angular", "Express", "Leaflet", "Pandas"],
       category: "Website",
-      date: "Oct, 2018 - Nov, 2018",
-      github: "https://github.com/hrishikeshpaul/whodata",
-      visit: "https://skylarktiral.herokuapp.com/",
+      date: "June, 2024 - Present",
+      github: "https://github.com/Lucirace/BharatWallet",
+      visit: "https://github.com/Lucirace/BharatWallet",
       description:
-        "This is a web application, uses a map to demonstrate the number suicides committed around the globe, according to a dataset provided by the WHO. The application consists of a Leaflet map that colors every nation, based on the number of suicides that have been committed in that country, for that year. It provides us with the capability to select the data as well as the continent.",
+        "I developed a digital wallet with peer-to-peer money transfer capabilities, using NextAuth for session-based authentication and Recoil for state management. I built an Express bank webhook server to simulate adding funds and implemented Docker and GitHub Actions for CI/CD, deploying to an Nginx reverse proxy. The deployment was directed to a DNS with an SSL certificate. I integrated Turborepo and used an AWS EC2 instance with Docker Hub. Prisma ORM was utilized for database handling and managing session transactions. ",
     },
     {
-      name: "Sentiment Analyzer",
+      name: "AI - Interview",
       pictures: [
         {
-          img: require("./src/assets/portfolio/smit/one.png"),
+          img: require("./src/assets/portfolio/noq/interview1.png"),
         },
         {
-          img: require("./src/assets/portfolio/smit/two.png"),
+          img: require("./src/assets/portfolio/noq/interview2.png"),
         },
         {
-          img: require("./src/assets/portfolio/smit/three.png"),
+          img: require("./src/assets/portfolio/noq/interview3.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/noq/interview4.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/noq/interview5.png"),
+        },
+      ],
+      technologies: [
+        " Next.js",
+        "Google Gemini API",
+        "PostgreSQL",
+        "Javascript",
+        "shadcn",
+      ],
+      category: "Website",
+      date: "April, 2024",
+      github: "https://github.com/Lucirace/AI-Interview",
+      visit: "https://ai-interview-tawny.vercel.app/",
+      description:
+        "I built an application using the Gemini API to create interview questions complete with ratings, answers, and feedback. Data management was handled by Drizzle ORM, which stores user answers. The application utilized speech-to-text recognition and camera input for enhanced interaction. Clerk Auth was implemented for secure and seamless user authentication.",
+    },
+    {
+      name: "Hivestar",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/post/hive1.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/post/hive2.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/post/hive3.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/post/hive3.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/post/hive5.png"),
+        },
+      ],
+      technologies: [
+        "Next.js",
+        "TypeScript",
+        "shadcn",
+        "Hive Blockchain",
+        "Hive Keychain",
+      ],
+      category: "Website",
+      github: "https://github.com/wiptrax/hivestar",
+      date: "15th April, 2024",
+      visit: "https://hivestar.vercel.app/",
+      description:
+        "My teammate and I achieved a Top 6 position and won prize money at the Vihaan007 Offline Hackathon among 1300+ participants. I developed Hivestar, a social media application on the Hive Blockchain using Hive Keychain for authentication, enabling users to view, vote, and upload posts stored on the blockchain, with media stored in Cloudinary. The project was implemented with Next.js, TypeScript, and shadcn, leveraging Hive.io, dhive, and Axios to fetch data from Hive Blog for the feed, extracting images using Regex.",
+    },
+    {
+      name: "EdgeInk",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/suicide/blog1.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/suicide/blog2.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/suicide/blog3.png"),
+        },
+      ],
+      technologies: [
+        "Typescript",
+        "Hono",
+        "Cloudflare Workers",
+        "React",
+        "PostgreSQL",
+        "Tailwind CSS",
+      ],
+      category: "Website",
+      date: "March, 2024",
+      github: "https://github.com/Lucirace/EdgeInk-BlogWebsite",
+      visit: "https://blog-website-steel-eight.vercel.app/signup",
+      description:
+        "I created a serverless blog application using Cloudflare, implementing JWT for authentication, Workers with Wrangler CLI, Zod for input validation, and Prisma Accelerate for connection pooling. The application features a rich-text editor for posting and viewing blogs with estimated read time. I also constructed an NPM package, used Hono for managing RESTful APIs, and stored data using Prisma ORM.",
+    },
+    {
+      name: "PaytmLite",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/smit/paytmLite1.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/smit/paytmLite2.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/smit/paytmLite3.png"),
         },
         {
           img: require("./src/assets/portfolio/smit/four.png"),
         },
       ],
-      technologies: ["LSTM", "Keras", "Angular", "Tweepy", "Heroku", "Python"],
-      category: "Alogrithm",
-      date: "Feb, 2018 - Apr  , 2018",
-      github: "https://github.com/hrishikeshpaul/sih",
-      visit: "https://angel.co/projects/1009777-sentiment-analysis",
+      technologies: [
+        "React",
+        "Javascript",
+        "Node.js",
+        "MongoDB Atlas",
+        "Tailwind CSS ",
+      ],
+      category: "Website",
+      date: "Feb, 2024",
+      github: "https://github.com/Lucirace/PaytmLite",
+      visit: "https://paytm-lite.vercel.app/signup",
       description:
-        "Developing this for the final round of Smart India Hackathon 2018, the web application is a platform for showcasing the sentiment of the tweets of the people in the Telangana region in India on policy satisfaction, services and problem definitions. The minimal UI design consists of different bins, that have their respective tweets, all scraped from twitter. There is also a simple graphical representation denoting the sentiment of the tweets.",
+        "I enhanced a money transfer application that assigns random dummy money to user accounts and enables peer-to-peer transfers using session transactions with Mongoose. The application was built with the Express framework, incorporating the CORS library, JWT for authentication, and Zod for input validation.",
     },
     {
-      name: "Clumpr",
+      name: "Portfolio",
       pictures: [
         {
-          img: require("./src/assets/portfolio/clumpr/logo.png"),
+          img: require("./src/assets/portfolio/clumpr/portfolio1.png"),
         },
         {
-          img: require("./src/assets/portfolio/clumpr/one.png"),
+          img: require("./src/assets/portfolio/clumpr/portfolio2.png"),
         },
         {
           img: require("./src/assets/portfolio/clumpr/two.png"),
         },
       ],
-      technologies: ["Node", "Angular", "Express", "MySQL", "Facebook API"],
-      category: "Web App",
-      github: "https://github.com/hrishikeshpaul/clumpr",
-      date: "Sep, 2017 - Nov, 2017",
-      visit: "https://github.com/hrishikeshpaul/clumpr",
+      technologies: ["Vue.js", "Javascript"],
+      category: "Website",
+      github: "https://github.com/Lucirace/Portfolio",
+      date: "June, 2024",
+      visit: "https://nipunbio.vercel.app/",
       description:
-        'Clumpr is an online social event planning assistant that - using information about similar events happening in similar locations around the same time, friends who frequently attend the same events, and mutual friends - helps users to collaborate, combine, and attend events together, effectively "clumping" people and activities in a way that makes intuitive sense.',
+        "My portfolio website using Vue.js and TypeScript, showcasing my projects, skills, and experience. The site features a clean, responsive design with dynamic content and smooth navigation, highlighting my expertise in web development.",
     },
-    {
+    /*  {
       name: "Nutricare",
       pictures: [
         {
@@ -337,6 +388,7 @@ let info = {
       description:
         "Nutricare is a basic health website for users(patients) to input nutrient intake data and researchers to analyse the data with the help of graphs and tables. A researcher can conduct studies on N different subjects (patient) for a particular problem statement. Typically, a researcher will enter food item consumed by subjects over a period of time (Ex - 24hrs, or 1 week, or 1 month etc).",
     },
+    */
   ],
   portfolio_design: [
     {
@@ -434,20 +486,23 @@ let info = {
   recommendations: [
     {
       title:
-        "In his internship, Hrishikesh has demonstrated excellent learning ability, and with a dedicated, task oriented approach, he was able to complete his assignment ahead of time.",
-      author: "Ushanas Shastri",
+        " Achieved a Top 6 position and prize money at the Vihaan007 Offline Hackathon among 1300+ participants in April 2024 for the Hive Blockchain Track ",
+      /*  author: "Ushanas Shastri",
       position: "CTO",
       company: "Viteos Capital Market Services",
       location: "Mumbai",
+    */
     },
     {
       title:
-        "I feel his budding leadership abilities will become even more effective in a diverse and challenging environment.",
-      author: "Anil Dukkipatty",
+        " Earned Google Cloud Skill Boost badge for completing the Google Cloud Program",
+      /*  author: "Anil Dukkipatty",
       position: "CTO",
       company: "Elemential Labs",
       location: "Mumbai",
+   */
     },
+    /*
     {
       title:
         "He has gained great knowledge and experience of SDE, and has learned ot develop the application keeping in mind the client's perpective and also making it creative.",
@@ -464,6 +519,7 @@ let info = {
       company: "Skylark Drones",
       location: "Bangalore",
     },
+    */
   ],
 };
 
